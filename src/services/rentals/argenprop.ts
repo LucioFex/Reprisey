b55a5fb441ca932642622bd9617b1324f634a344
= <=>
2023-01-02 19:30:50 -0300

    feat (incomplete): Add of processing for the rentals fetch

--- src/services/rentals/argenprop.ts
+++ src/services/rentals/argenprop.ts
@@ -1,4 +1,4 @@
-// import cheerio from 'cheerio';
+import cheerio from 'cheerio';
 import axios from 'axios';
 import { IArgenpropData, ObjectStNu } from '../../types';
 
@@ -20,21 +20,57 @@ const zoneFormatter = (zoneData: ObjectStNu): string[] => {
     return [zoneType, zoneName];
 };
 
-const getRentalsInZone = async (zoneType: string, zoneName: string): Promise<string[]> => {
+const getRentalsInZone = async (zoneType: string, zoneName: string): Promise<string> => {
     /* Fetch of rentals in the zone selected */
     const url = `https://www.argenprop.com/departamento-alquiler-${zoneType}-${zoneName}`;
     const fetchDom = await axios.get(url, { headers: { 'Accept-Encoding': 'gzip,deflate,compress' } });
 
-    const { data }: { data: string[] } = fetchDom;
+    const { data }: { data: string } = fetchDom;
     return data;
 };
 
-const getArgenprop = async (location: string, query: ObjectStNu): Promise<IArgenpropData[]> => {
+const processRentalData = (data): IArgenpropData => { // TO DO
+    /* Object formatting of the different rentals in the search */
+    const rentalData: IArgenpropData = {};
+    const $ = cheerio.load(data);
+
+    const costs: string[] = $('.card__price').text().split('+');
+
+    // Filter of the 'Consultar precio' rentals
+    if (costs[0].includes('Consultar precio')) throw Error('"Consultar precio" rental');
+
+    const [price, expenses]: string[] = [
+        costs[0].trim().slice(2).split('.').join(''), // Done
+        costs[1].trim().slice(1).split('.').join(''), // Need Fix
+    ];
+
+    console.log([price, expenses]);
+
+    return { location: 'dummy info' };
+};
+
+const organizeRentalsData = (rawHtmlData: string): IArgenpropData[] => {
+    const organizedData: IArgenpropData[] = [];
+    const $ = cheerio.load(rawHtmlData);
+
+    $('.listing__item').each((_index, elem) => {
+        try {
+            organizedData.push(processRentalData(elem));
+        } catch (err) {
+            // console.error('Rental skiped\n', err); // Rental skip
+        }
+    });
+    return organizedData;
+};
+
+const getArgenprop = async (location: string, filters: ObjectStNu): Promise<IArgenpropData[]> => {
     const value: ObjectStNu = await websiteSeachApi(location);
     const [zoneType, zoneName]: string[] = zoneFormatter(value);
-    const rawDomData: string[] = await getRentalsInZone(zoneType, zoneName); // To Do
 
-    return [{ location: 'sample' }];
+    const rawHtmlData: string = await getRentalsInZone(zoneType, zoneName);
+    const organizedData: IArgenpropData[] = organizeRentalsData(rawHtmlData);
+
+    return organizedData;
 };
 
 export default getArgenprop;
